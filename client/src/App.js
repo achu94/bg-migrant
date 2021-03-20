eee0219a5969289547cdbeaf032b6ef124931725
achu94 <aygyun_94@outlook.de>
2021-03-20 16:27:19 +0100

    navbar made better.

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,31 @@ import style from './App.module..css';
 
 function App() {
   return (
-    <div className={style.app}>
+    <div className="app">
       <nav className="navigation" >
-        <div class="logo">
-          <a tabindex="-1" href="/">
-            BG-MIGRANT
-          </a>
-          </div>
+        <div className="nav-header-1" >
+          <span class="logo">
+            <a tabindex="-1" href="/">
+              BG-MIGRANT
+            </a>
+          </span>
+
+          <form action="/" id="search-form" method="get" style={{ width: "50%" }} >
+            <input className="search-bar" type="text" style={{ width: "100%" }} />
+          </form>
+
+          <ul >
+            <li className=""><a href="#">Вход</a></li>
+            <li className=""><a href="#">Регистрация</a></li>
+          </ul>
+
+        </div>
         <ul>
           <li className="listItem"><a href="#">Документи</a></li>
           <li className="listItem"><a href="#">Новини</a></li>
-          <li className="listItem"><a href="#">Вход</a></li>
-          <li className="listItem"><a href="#">Регистрация</a></li>
+          <li className="listItem"><a href="#">Работа</a></li>
+          <li className="listItem"><a href="#">Aпартамент</a></li>
+          <li className="listItem"><a href="#">Български в Германия</a></li>
         </ul>
       </nav>
     </div>
