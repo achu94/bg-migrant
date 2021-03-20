7d0b442ffa128cc93029d6f9eb045779206993f9
achu94 <aygyun_94@outlook.de>
2021-03-20 15:33:38 +0100

    Navbar was added to App.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
-import './App.css';
+import style from './App.module..css';
 
 function App() {
   return (
     <div className={style.app}>
+      <div id="logo">
+      <a tabindex="-1" href="/">
+        BG-MIGRANT
+      </a>
+    </div>
+      <nav className="navigation" >
+        <ul>
+          <li className="listItem"><a href="#">Документи</a></li>
+          <li className="listItem"><a href="#">Новини</a></li>
+          <li className="listItem"><a href="#">Вход</a></li>
+          <li className="listItem"><a href="#">Регистрация</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
