1920e230744e037ede61ceb4079a5758277b2e10
achu94 <aygyun_94@outlook.de>
2021-03-23 17:59:04 +0100

    New Components has been added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,13 @@
 import style from './App.module..css';
 
+import Navbar from './components/Header/Navbar';
+import NewsList from './components/News/NewsList';
+
 function App() {
   return (
     <div className="app">
-      <nav className="navigation" >
-        <div className="nav-header-1" >
-          <span class="logo">
-            <a tabindex="-1" href="/">
-              BG-MIGRANT
-            </a>
-          </span>
-
-          <form action="/" id="search-form" method="get" style={{ width: "50%" }} >
-            <input className="search-bar" type="text" style={{ width: "100%" }} />
-          </form>
-
-          <ul >
-            <li className="listItem-2"><a href="#">Регистрация</a></li>
-            <li className="listItem-2"><a href="#">Вход</a></li>
-          </ul>
-
-        </div>
-        <ul>
-          <li className="listItem"><a href="#">Фoruм</a></li>
-          <li className="listItem"><a href="#">Новини</a></li>
-          <li className="listItem"><a href="#">Работа</a></li>
-          <li className="listItem"><a href="#">Kвиртира</a></li>
-          <li className="listItem"><a href="#">Българскo в Германия</a></li>
-        </ul>
-      </nav>
+      <Navbar />
+      <NewsList />
     </div>
   );
 }
---
+++ client/src/components/User/Login/Login.js
@@ -0,0 +1,9 @@
+import './Login.css';
+
+const Login = () => {
+    return(
+        <li className="listItem-2"><a href="#">Вход</a></li>
+    );
+}
+
+export default Login;
