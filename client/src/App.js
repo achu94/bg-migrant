c0ce533426bd91da21d727cd4d69cadff87fd0e7
Aygyun <49846915+achu94@users.noreply.github.com>
2021-03-23 18:48:42 +0100

    Router added to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
-import style from './App.module..css';
+import { Route, Link, NavLink, Redirect, Switch } from "react-router-dom";
 
-import Navbar from './components/Header/Navbar';
-import NewsList from './components/News/NewsList';
+import Navbar from "./components/Header/Navbar";
+
+import Register from "./components/User/Register";
+import Login from "./components/User/Login";
+
+import Forum from "./components/Forum";
+import News from "./components/News";
+import Work from "./components/Work";
+import Flat from "./components/Flat";
+import Specpage from "./components/Specpage";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
-      <NewsList />
+      <Switch>
+        <Route path="/" exact />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+
+        <Route path="/forum" component={Forum} />
+        <Route path="/news" component={News} />
+        <Route path="/work" component={Work} />
+        <Route path="/flat" component={Flat} />
+        <Route path="/specpage" component={Specpage} />
+      </Switch>
     </div>
   );
 }
