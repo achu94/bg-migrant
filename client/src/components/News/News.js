dbad3429965a47f7bc509843cea92f119215d20b
achu94 <aygyun_94@outlook.de>
2021-03-24 17:49:27 +0100

    Fetch to server to get news and render.

--- client/src/components/News/News.js
+++ client/src/components/News/News.js
@@ -1,27 +1,59 @@
-import { Redirect, useHistory } from 'react-router-dom';
+import { Component } from 'react';
 
-const News = ({
-    match
-}) => {
+import './News.css';
 
-    const history = useHistory();
+class News extends Component {
 
-    // if(match.params.country === 'bg'){
-    //     return <Redirect to="/" />
-    // }
-
-    if(match.params.country === 'bg'){
-        history.push("/")
-        return null;
+    state = {
+        isLoading: true,
+        newsData: [],
+        error: null
     }
 
-    console.log(match.params.country)
+    fetchNews() {
+        // Where we're fetching data from
+        fetch('http://localhost:5000/news')
+            .then(response => response.json())
+            // ...then we update the users state
+            .then(data =>
+                this.setState({
+                    newsData: data,
+                    isLoading: false,
+                })
+            )
+            // Catch any errors we hit and update the app
+            .catch(error => this.setState({ error, isLoading: false }));
+    }
 
-    return(
-        <h1>
-            News Page : {match.params.country}
-        </h1>
-    );
+    componentDidMount() {
+        this.fetchNews();
+    }
+
+    render() {
+        const { isLoading, newsData, error } = this.state;
+        return (
+            <articles className="main">
+                <h1>Новини от България</h1>
+                {error ? <p>{error.message}</p> : null}
+
+                {!isLoading ? (
+                    newsData.map(news => {
+                        const { title, author,urlToImage,url, publishedAt } = news;
+                        return (
+                            <article className="article" key={title}>
+                                <img src={urlToImage} alt=""/>
+                                <a target="__blank" href={url}><h3>{title}</h3></a>
+                                <p>Author: {author}</p>
+                                <p>публикувано: {publishedAt}</p>
+                            </article>
+                        );
+                    })
+                ) : (
+                    <h3>Loading...</h3>
+                )}
+            </articles>
+        )
+    }
 }
 
 export default News;
