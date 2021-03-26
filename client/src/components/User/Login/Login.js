b1a5e61ad40ce8bbe4b23508ca281f18ac8e4ec4
Aygyun <49846915+achu94@users.noreply.github.com>
2021-03-26 12:17:41 +0100

    Working on Login.

--- client/src/components/User/Login/Login.js
+++ client/src/components/User/Login/Login.js
@@ -1,8 +1,25 @@
+import '../Register/Register.css';
+
+const onSubmitLoginHandler = (e) => {
+    e.preventDefault();
+
+    console.log(e.target.username.value);
+    console.log(e.target.password.value);
+}
+
 const Login = () => {
     return(
-        <h1>
-            Login Page
-        </h1>
+        <div className="main">
+            <form onSubmit={onSubmitLoginHandler}  className="register-form" >
+                <label htmlFor="username">Потребителско име</label>
+                <input type="text" id="username" name="username" />
+
+                <label htmlFor="password">Парола</label>
+                <input type="password" id="password" name="password" />
+
+                <input type="submit" className="register-send" value="изпрати" />
+            </form>
+        </div>
     );
 }
 
