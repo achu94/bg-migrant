79e4a159211f1bdde3eddd5532416d4c4e2effed
Aygyun <49846915+achu94@users.noreply.github.com>
2021-03-26 12:17:49 +0100

    Working on Register.

--- client/src/components/User/Register/Register.js
+++ client/src/components/User/Register/Register.js
@@ -1,8 +1,48 @@
+import React, { useState } from 'react';
+
+import './Register.css';
+
 const Register = () => {
+
+    const [{isValid}, setValidation] = useState(false);
+    const [email, setEmail] = useState('абв@абв.бг')
+
+    const onSubmitRegisterHandler = (e) => {
+        e.preventDefault();
+    
+        console.log(e.target.username.value);
+        console.log(e.target.password.value);
+        console.log(e.target["password-rep"].value);
+        console.log(e.target["register-email"].value);
+    
+        if(e.target["register-email"].value){
+            setValidation( {isValid: true});
+        }
+    }
+
+    const onEmailChangeHandler = (e) => {
+        e.preventDefault();
+        setEmail(e.target.value);        
+    };
+
     return(
-        <h1>
-            Register Page
-        </h1>
+        <div className="main">
+            <form onSubmit={onSubmitRegisterHandler}  className="register-form" >
+                <label htmlFor="username">Потребителско име</label>
+                <input type="text" id="username" name="username" />
+
+                <label htmlFor="password">Парола</label>
+                <input type="password" id="password" name="password" />
+
+                <label htmlFor="password-rep">Повторете паролата</label>
+                <input type="password" id="password-rep" name="password-rep" />
+
+                <label htmlFor="register-email">Електронна поща</label>
+                <input type="email" id="register-email" name="register-email" placeholder={email} onChange={onEmailChangeHandler} />
+
+                <input type="submit" className="register-send" value="изпрати" />
+            </form>
+        </div>
     );
 }
 
