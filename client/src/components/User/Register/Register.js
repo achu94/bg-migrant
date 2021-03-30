fd99431a4d0d639421f1471c6a600b36e63c272a
achu94 <aygyun_94@outlook.de>
2021-03-30 14:29:54 +0200

    User Register Front-End has been added.

--- client/src/components/User/Register/Register.js
+++ client/src/components/User/Register/Register.js
@@ -1,46 +1,63 @@
 import React, { useState } from 'react';
+import {Post} from './Services/registerPost';
 
 import './Register.css';
 
 const Register = () => {
 
-    const [{isValid}, setValidation] = useState(false);
-    const [email, setEmail] = useState('абв@абв.бг')
+    const [{ isValid }, setValidation] = useState(false);
+    const [email, setEmail] = useState('абв@абв.бг');
+
+    // const [userNameValidation, setUserNameValidation]               = useState(false);
+    // const [userPasswordValidation, setUserPasswordValidation]       = useState(false);
+    // const [userPasswordRepValidation, setUPasswordRepameValidation] = useState(false);
+    // const [userEmailValidation, setUEmailameValidation]             = useState(false);
+
+    const [sendDisabled, setSendDisabled] = useState(false);
+
+    const onBlurErrorHandler = (e) => {
+
+    }
 
     const onSubmitRegisterHandler = (e) => {
         e.preventDefault();
-    
-        console.log(e.target.username.value);
-        console.log(e.target.password.value);
-        console.log(e.target["password-rep"].value);
-        console.log(e.target["register-email"].value);
-    
-        if(e.target["register-email"].value){
-            setValidation( {isValid: true});
-        }
+
+        // console.log(e.target.username.value);
+        // console.log(e.target.password.value);
+        // console.log(e.target["password-rep"].value);
+        // console.log(e.target["register-email"].value);
+
+        const userData = {
+            "username" : e.target.username.value,
+            "password" : e.target.password.value,
+            "repaePassword" : e.target["password-rep"].value,
+            "eMail" : e.target["register-email"].value
+        };
+        
+        Post(userData);
     }
 
     const onEmailChangeHandler = (e) => {
         e.preventDefault();
-        setEmail(e.target.value);        
+        setEmail(e.target.value);
     };
 
-    return(
+    return (
         <div className="main">
-            <form onSubmit={onSubmitRegisterHandler}  className="user-form" >
-                <label htmlFor="username">Потребителско име</label>
-                <input type="text" id="username" name="username" />
+            <form onSubmit={onSubmitRegisterHandler} className="user-form" >
+                <label htmlFor="username">Потребителско име*</label>
+                <input onBlur={onBlurErrorHandler} type="text" id="username" name="username" />
 
-                <label htmlFor="password">Парола</label>
-                <input type="password" id="password" name="password" />
+                <label htmlFor="password">Парола*</label>
+                <input onBlur={onBlurErrorHandler} type="password" id="password" name="password" />
 
-                <label htmlFor="password-rep">Повторете паролата</label>
-                <input type="password" id="password-rep" name="password-rep" />
+                <label htmlFor="password_rep">Повторете паролата*</label>
+                <input onBlur={onBlurErrorHandler} type="password" id="password-rep" name="password-rep" />
 
-                <label htmlFor="register-email">Електронна поща</label>
-                <input type="email" id="register-email" name="register-email" placeholder={email} onChange={onEmailChangeHandler} />
+                <label htmlFor="register_email">Електронна поща*</label>
+                <input onBlur={onBlurErrorHandler} type="email" id="register-email" name="register-email" placeholder={email} onChange={onEmailChangeHandler} />
 
-                <input type="submit" className="user-send" value="изпрати" />
+                <input disabled={sendDisabled} type="submit" className="user-send" value="изпрати" />
             </form>
         </div>
     );
---
+++ client/src/components/User/Register/Services/registerPost.js
@@ -0,0 +1,23 @@
+import { Redirect } from 'react-router-dom';
+import Login from '../../Login/Login';
+
+const Post = (userData) => {
+
+    // Simple POST request with a JSON body using fetch
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(userData)
+    };
+
+    fetch('http://localhost:5000/user/register', requestOptions)
+        .then(res => res.text())
+        .then( (res) => {
+            console.log(res);
+            return window.location.href = '/login';            
+        });
+}
+
+export {
+    Post,
+}
