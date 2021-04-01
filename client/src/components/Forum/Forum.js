be9b5ad2b1a2708c1f0a3c11a68049eb3296c32f
achu94 <aygyun_94@outlook.de>
2021-04-01 05:16:13 +0200

    Forum Dummy.

--- client/src/components/Forum/Forum.js
+++ client/src/components/Forum/Forum.js
@@ -1,8 +1,51 @@
+import './Forum.css';
+
 const Forum = () => {
-    return(
-        <h1>
-            Forum Page
-        </h1>
+
+    const data = [
+        {
+            "header" : 'Hallo',
+            "shortText" : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet provident vitae ullam illo libero aspernatur corrupti rem maxime ab. Ratione.',
+            "user" : 'achu',
+        },
+        {
+            "header" : 'Malo',
+            "shortText" : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet provident vitae ullam illo libero aspernatur corrupti rem maxime ab. Ratione.'
+        },
+        {
+            "header" : 'Tallo',
+            "shortText" : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet provident vitae ullam illo libero aspernatur corrupti rem maxime ab. Ratione.'
+        },
+        {
+            "header" : 'coco',
+            "shortText" : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet provident vitae ullam illo libero aspernatur corrupti rem maxime ab. Ratione.'
+        },
+
+    ];
+
+    return (
+        <div className="main">
+            <div className="forum-header">
+                <h1>Alle Fragen</h1>
+                <button>Eine Frage stellen</button>
+            </div>
+
+            <ul className="forum-list" >
+                {
+                    data.map( (theme) => {
+                        return( 
+                        <li>
+                            <div className="list-item-photo" ></div> 
+                            <div>
+                                <h3 className="forum-list-header" >{` ${theme.header} `}</h3>
+                                <p className="forum-list-short-text">{` ${theme.shortText} `}</p>
+                                <p className="forum-list-user">{` ${theme.user} `}</p>
+                            </div> 
+                        </li>)
+                    })
+                }
+            </ul>
+        </div>
     );
 }
 
